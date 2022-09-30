67e4e936cec51d0d5c7b3aa3c544b6795be2aa10
junji oishi <junji.oishi@nijibox.co.jp>
2022-09-30 17:35:01 +0900

    react-test-atomicdesign
    AtomicDesignのテスト①

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/Searchinput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  name: "名無し",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "050-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+}
+
 function App() {
   return (
+    <>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <SearchInput />
+      <UserCard user={user} />
+    </>
 
   );
 }
