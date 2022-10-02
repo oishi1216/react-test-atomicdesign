a3cb182132c055e0ac3c387bfce00dd82b1dfbee
junji oishi <abdaqoopmhm@gmail.com>
2022-10-02 10:09:04 +0900

    react-test-atomicdesign
    グローバルなstateの追加

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
     const navigation = useNavigate();
-    const onClickAdmin = () => navigation("users", {state: {isAdmin: true}});
-    const onClickGeneral = () => navigation("users", {state: {isAdmin: false}});
+    const { setUserInfo } = useContext(UserContext);
+
+    const onClickAdmin = () => {
+        setUserInfo({ isAdmin: true });
+        navigation("users");
+    }
+    const onClickGeneral = () => {
+        setUserInfo({ isAdmin: false });
+        navigation("users");
+    }
     return(
         <SContainer>
             <h2>TOPページです</h2>
